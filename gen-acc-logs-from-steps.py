8aa873dbda3818606ea809f13c9e4b4630e143cb
roozbeh <roozbeh@us.ibm.com>
2017-10-31 22:00:49 -0400

    Add the challenge 10 steps and brush the code

--- gen-acc-logs-from-steps.py
+++ gen-acc-logs-from-steps.py
@@ -4,6 +4,43 @@ import numpy as np
 start_col = 10 # counting from 0
 end_col = 40 # counting from 0
 
+# ----------------------------
+
+def make_person_acc_values(inputrow):
+    name = inputrow[1]
+    name = name.replace('.', '')
+    names.append(name)
+    
+    # read the values from the spreadsheet value (after the column for total)
+    str_values = inputrow[end_col+2:]
+    
+    acc_values = []
+    acc_value = 0.0
+    for i in range(len(str_values)):
+        if str_values[i] == '':
+            appendee = None
+            #break
+        else:
+            # convert the strings to float
+            values = [float(x) for x in str_values[0:i+1]]
+            
+            # for the first one we do not have minimum and so we treat it differently
+            if i==0:
+                acc_value = acc_value + values[i]
+                count_total = 1
+            else:
+                acc_value = sum(values) - min(values)
+                count_total = i
+            
+            # convert accumulated values to average value steps
+            appendee = np.exp(1.0 * acc_value / count_total) * 10000.0
+            
+        acc_values.append(appendee)
+        
+    return acc_values
+
+# ----------------------------
+
 with open('Challenge.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
@@ -11,41 +48,14 @@ with open('Challenge.csv', 'rb') as csvfile:
     dates = first[start_col:end_col+1]
 
     names = ['date'] # the d3 visualization needs this
-    all_acc_logs = []
+    all_acc_values = []
 
     for inputrow in csvreader:
-        name = inputrow[1]
-        name = name.replace('.', '')
-        names.append(name)
-
-        logs = inputrow[end_col+2:]
-
-        acc_logs = []
-        acc_log = 0.0
-        for i in range(len(logs)):
-            if logs[i] == '':
-                appendee = None
-                #break
-            else:
-                # convert the strings to float
-                log_values = [float(x) for x in logs[0:i+1]]
-
-                # for the first one we do not have minimum and so we treat it differently
-                if i==0:
-                    acc_log = acc_log + log_values[i]
-                    count_total = 1
-                else:
-                    acc_log = sum(log_values) - min(log_values)
-                    count_total = i
-
-                # convert accumulated logs to average log steps
-                appendee = np.exp(1.0 * acc_log / count_total) * 10000.0
-
-            acc_logs.append(appendee)
-
-        all_acc_logs.append(acc_logs)
-
-    all_acc_logs_transpose = zip(*all_acc_logs)
+        person_acc_values = make_person_acc_values(inputrow)
+            
+        all_acc_values.append(person_acc_values)
+        
+    all_acc_values_transpose = zip(*all_acc_values)
 
     with open('acc-data.tsv', 'wb') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter='\t',
@@ -55,6 +65,6 @@ with open('Challenge.csv', 'rb') as csvfile:
 
         for i in range(len(dates)):
             writing_row = [dates[i]]
-            writing_row = writing_row + list(all_acc_logs_transpose[i])
+            writing_row = writing_row + list(all_acc_values_transpose[i])
 
             csvwriter.writerow(writing_row)
