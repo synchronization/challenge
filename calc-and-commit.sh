a7948ca2c94fdec470a142d3f355386c0beef70d
roozbeh <roozbeh@us.ibm.com>
2018-11-21 09:36:46 -0500

    Use echo messages

--- calc-and-commit.sh
+++ calc-and-commit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# get the steps and calculate the progress
+echo "get the steps and calculate the progress"
 python generate-analytics.py
 
-# make the commit message
+echo "make the commit message"
 commitmessage="Update the steps on $(date)"
 
-# commit and push
+echo "commit and push"
 git commit acc-data.tsv Challenge.csv -m "$commitmessage"
 git push
 
